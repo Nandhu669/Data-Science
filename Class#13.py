0382ed44dd7860b5872c333f5d86d8868bfab769
Nandhu669 <mrnandhu710@gmail.com>
2026-07-10 08:24:39 +0530

    13th class added

---
+++ Class#13.py
@@ -0,0 +1,49 @@
+#dictionary
+d = {"Name":"Nandhu",
+     "Age":22,
+     "City":"Kollam",
+     "Hobby":"Football"}
+# print(d)
+# print(d["Name"])    #to get value
+# print(len(d))
+# print(type(d))
+# a = {}
+# print(type(a))
+# print(d.get("Age")) #to get value
+# d["Fav:"] = "Anime"  # adds key
+# print(d)
+# d.update({"Hobby":"Badminton"}) #update values
+# print(d)
+# d.setdefault("Game","Lord of rings") #adds if not exist
+# print(d)
+# d.pop('Game')
+# print(d)
+# d.popitem()  #removes last pair
+# print(d)
+# del d["Age"]
+# print(d)
+# d.clear()
+# print(d)
+# print(d.keys()) #to find key values
+# print(d.copy())  #copy dictionary
+# print(dict([(1,"Anu")])) #conversion
+# print("Age"in d)  #key check in
+# print("Age" not in d) #key check not in
+
+# for i in d:
+#     print(d)
+# for j in d.values():
+#     print(d)
+# for i,j in d.items():
+#     print(d)
+
+
+#problems
+x = [10,2,5,[2,4,6],21,48]
+y = {1:"A",2:"B",4:{'Vowels':{'a','e','i','o','u'}},3:'c'}
+print(x[3][1])
+print(y[4]['Vowels'])
+m = y[4]['Vowels']
+print(m)
+
+
